1c1ede9c147e9cffc9bc3f945520cf40f10d83de
edayaxin <yaxinhu94@gmail.com>
2023-10-23 23:48:37 -0500

    adjust control

--- ros2-bridge/rosbridge_converter.py
+++ ros2-bridge/rosbridge_converter.py
@@ -9,10 +9,10 @@ from sensor_msgs.msg import Joy
 host='54.90.73.9'
 port=9090
 client = roslibpy.Ros("ws://%s:%d" % (host, port))
-if (client.is_connected): 
-    client.run()
-else: 
-    print("ros bridge not connected")
+# if (client.is_connected): 
+#     client.run()
+# else: 
+#     print("ros bridge not connected")
 client.run()
 
 class MinimalPublisher(Node):
@@ -21,7 +21,7 @@ class MinimalPublisher(Node):
         super().__init__('speedPublisher')
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
         self.speed = Twist()
-        timer_period = 0.1  # seconds
+        timer_period = 0.1 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
@@ -35,8 +35,10 @@ class MinimalPublisher(Node):
         self.joySub
 
     def timer_callback(self):
+        self.speed.linear.x = round(self.speed.linear.x, 4)
+        self.speed.angular.z = round(self.speed.angular.z, 4)
         self.publisher.publish(self.speed)
-        self.get_logger().info('Publishing cmd_vel: "%s"' % self.speed)
+        # self.get_logger().info('Publishing cmd_vel: "%s"' % self.speed)
         self.i += 1
 
     # remote control (joystick or keyboard) callback
@@ -60,24 +62,44 @@ class MinimalPublisher(Node):
             self.move_backward()
         if (message['data'] == "stop"):
             self.stop()
+        if (message['data'] == "left_slow"):
+            self.move_left_slow()
+        if (message['data'] == "right_slow"):
+            self.move_right_slow()
     
     def move_left(self):
-        self.speed.angular.z = min(self.speed.angular.z + 0.01, 1)
+        self.get_logger().info('Go left %f' % self.speed.angular.z )
+        self.speed.angular.z = min(self.speed.angular.z + 0.05, 1)
     def move_right(self):
-        self.speed.angular.z = max(self.speed.angular.z - 0.01, -1)
+        self.get_logger().info('Go right %f' % self.speed.angular.z )
+        self.speed.angular.z = max(self.speed.angular.z - 0.05, -1)
     def move_forward(self):
+        self.get_logger().info('Go forward %f' % self.speed.linear.x)
         self.speed.linear.x = min(self.speed.linear.x + 0.005, 0.25)
     def move_backward(self):
+        self.get_logger().info('Go backward %f' % self.speed.linear.x)
         self.speed.linear.x = max(self.speed.linear.x - 0.005, -0.25)
 
+    def move_left_slow(self):
+        self.get_logger().info('Go left slow %f' % self.speed.angular.z )
+        self.speed.angular.z = min(self.speed.angular.z + 0.01, 1)
+
+    def move_right_slow(self):
+        self.get_logger().info('Go right slow %f' % self.speed.angular.z )
+        self.speed.angular.z = max(self.speed.angular.z - 0.01, -1)
+
     def move_left_cmd(self):
-        self.speed.angular.z = 0.5
+        # self.speed.linear.x = 0
+        self.speed.angular.z = 0.1
     def move_right_cmd(self):
-        self.speed.angular.z = -0.5
+        # self.speed.linear.x = 0
+        self.speed.angular.z = -0.1
     def move_forward_cmd(self):
-        self.speed.linear.x = 0.1
+        self.speed.angular.z = 0
+        self.speed.linear.x = 0.05
     def move_backward_cmd(self):
-        self.speed.linear.x = -0.1
+        self.speed.angular.z = 0
+        self.speed.linear.x = -0.05
 
     def stop(self):
         self.speed = Twist()
@@ -86,7 +108,6 @@ class MinimalPublisher(Node):
     def joy_callback(self, message):
         if (message.axes[6] == 1): 
             self.move_left()
-            self.get_logger().info('Joystick pressed: left %f' % self.speed.angular.z )
         if (message.axes[6] == -1):
             self.move_right()
             self.get_logger().info('Joystick pressed: right %f' % self.speed.angular.z)
@@ -100,6 +121,13 @@ class MinimalPublisher(Node):
         if (message.buttons[7] == 1 or message.buttons[9] == 1): #right buttons edge
             self.get_logger().info('Joystick pressed: stop %s' % message.buttons)
             self.stop()
+            
+        if (message.buttons[3] == 1):  
+            self.get_logger().info('Joystick pressed: left slow %s' % message.buttons)
+            self.move_left_slow()
+        if (message.buttons[1] == 1):  
+            self.get_logger().info('Joystick pressed: right slow %s' % message.buttons)
+            self.move_right_slow()
             
 rclpy.init()
 speedPublisher = MinimalPublisher()
